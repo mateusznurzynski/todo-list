e081b4af38ad42e92926506838546789630b145f
Mateusz Nurzyński <mateonurzynski@gmail.com>
2022-12-03 17:44:59 +0100

    Add a modal that allows the user to create projects

--- src/components/dom-control.js
+++ src/components/dom-control.js
@@ -1,20 +1,48 @@
 import PubSub from 'pubsub-js';
 import Todo from './todos';
+import Project from './projects';
 import { createDomElement } from '../utils/utilities';
+import addProjectIcon from '../icons/add-project.svg';
 
 export default (function DomControl() {
 	const todoFormElement = document.querySelector('.todo-form');
 	const todosElement = document.querySelector('.todos');
 
+	const projectFormElement = document.querySelector('.project-form');
+	const images = [
+		{
+			src: addProjectIcon,
+			location: document.querySelector('.add-project'),
+			alt: 'Add new project',
+			className: 'add-project-img',
+		},
+	];
+
 	function initPage() {
+		loadImages();
 		addListeners();
 	}
 
 	function addListeners() {
-		todoFormElement.addEventListener('submit', (e) => {
+		// todoFormElement.addEventListener('submit', (e) => {
+		// 	e.preventDefault();
+		// 	const data = new FormData(todoFormElement);
+		// 	Todo.addTodo(data);
+		// });
+		projectFormElement.addEventListener('submit', (e) => {
 			e.preventDefault();
-			const data = new FormData(todoFormElement);
-			Todo.addTodo(data);
+			const data = new FormData(projectFormElement);
+			Project.createProject(data);
+		});
+	}
+
+	function loadImages() {
+		images.forEach((image) => {
+			const img = new Image();
+			img.src = image.src;
+			img.alt = image.alt;
+			img.classList.add(image.className);
+			image.location.appendChild(img);
 		});
 	}
 
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,8 +3,15 @@ import PubSub from 'pubsub-js';
 export default (function Project() {
 	const projects = [];
 
-	function createProject(name) {
-		projects.push(Object.assign({}, { name }));
+	function createProject(data) {
+		const state = {
+			name: data.get('projectName'),
+		};
+
+		projects.push(Object.assign({}, state));
 		PubSub.publish('projectsChanged', projects);
+		console.log(projects);
 	}
+
+	return { createProject };
 })();
